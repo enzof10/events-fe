9cb3d89555408229553eb66f0ed945045eee9a7d
enzo frias borda <enzo_17_05@hotmail.com>
2023-11-09 18:34:27 -0300

    add .next to gitIgnore

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -6,63 +6,65 @@ import NavLink from "../core/NavLink";
 
 //* NavBar Component
 const NavBar: FC = () => {
-  const { pathname } = useRouter();
+    const { pathname } = useRouter();
 
-  return (
-    <div
-      className={
-        "navbar duration-100  w-full " +
-        (pathname === "/"
-          ? "bg-transparent absolute top-0 z-20"
-          : "bg-orange-600/90 block")
-      }
-    >
-      <header className="text-gray-600 body-font">
-        <div className="container flex items-center justify-between px-4 py-3 mx-auto">
-          <Link href="/">
-            <a className="inline-flex items-center text-lg font-semibold gap-x-2">
-              <img
-                src="/images/logo.png"
-                className="w-10 h-10"
-                alt="Website Logo"
-              />
-              <span className="w-40 text-xl font-bold text-white">Events</span>
-            </a>
-          </Link>
-          <nav className="flex items-center justify-center gap-3 text-base md:ml-auto">
-            <NavLink
-              styleName="p-[10px] md:p-[13px] font-semibold duration-300 rounded-xl bg-transparent text-white hover:bg-slate-800/90"
-              href={"/"}
-              activePath={"/"}
-            >
-              Home
-            </NavLink>
-            <NavLink
-              styleName="p-[10px] md:p-[13px] font-semibold duration-300 rounded-xl bg-transparent text-white hover:bg-slate-800/90"
-              href={"/events"}
-              activePath={"/events"}
-            >
-              Events
-            </NavLink>
-            <a
-              href="https://github.com/ahmedmohmd/events-app"
-              target="_blank"
-              rel="noreferrer"
-              className="items-center justify-center hidden pt-3 border-t-2 md:flex project-link sm:pt-0 sm:pl-3 sm:border-l-2 sm:border-t-0 border-slate-200/50"
-            >
-              <div className="p-3 duration-300 rounded-full hover:bg-slate-800/90">
-                <img
-                  src="/images/github-icon.svg"
-                  className="fil"
-                  alt="Github Icon"
-                />
-              </div>
-            </a>
-          </nav>
+    return (
+        <div
+            className={
+                "navbar duration-100  w-full " +
+                (pathname === "/"
+                    ? "bg-transparent absolute top-0 z-20"
+                    : "bg-orange-600/90 block")
+            }
+        >
+            <header className="text-gray-600 body-font">
+                <div className="container flex items-center justify-between px-4 py-3 mx-auto">
+                    <Link href="/">
+                        <a className="inline-flex items-center text-lg font-semibold gap-x-2">
+                            <img
+                                src="/images/logo.png"
+                                className="w-10 h-10"
+                                alt="Website Logo"
+                            />
+                            <span className="w-40 text-xl font-bold text-white">
+                                Events
+                            </span>
+                        </a>
+                    </Link>
+                    <nav className="flex items-center justify-center gap-3 text-base md:ml-auto">
+                        <NavLink
+                            styleName="p-[10px] md:p-[13px] font-semibold duration-300 rounded-xl bg-transparent text-white hover:bg-slate-800/90"
+                            href={"/"}
+                            activePath={"/"}
+                        >
+                            Home
+                        </NavLink>
+                        <NavLink
+                            styleName="p-[10px] md:p-[13px] font-semibold duration-300 rounded-xl bg-transparent text-white hover:bg-slate-800/90"
+                            href={"/events"}
+                            activePath={"/events"}
+                        >
+                            Events
+                        </NavLink>
+                        <a
+                            href="https://github.com/enzof10"
+                            target="_blank"
+                            rel="noreferrer"
+                            className="items-center justify-center hidden pt-3 border-t-2 md:flex project-link sm:pt-0 sm:pl-3 sm:border-l-2 sm:border-t-0 border-slate-200/50"
+                        >
+                            <div className="p-3 duration-300 rounded-full hover:bg-slate-800/90">
+                                <img
+                                    src="/images/github-icon.svg"
+                                    className="fil"
+                                    alt="Github Icon"
+                                />
+                            </div>
+                        </a>
+                    </nav>
+                </div>
+            </header>
         </div>
-      </header>
-    </div>
-  );
+    );
 };
 
 export default NavBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,65 +5,66 @@ import { getFeaturedEvents } from "../utils/db";
 import type { Event } from "../utils/types";
 
 interface HomePageProps {
-  events: Event[];
+    events: Event[];
 }
 
 const HomePage: NextPage<HomePageProps> = ({ events }) => {
-  return (
-    <div>
-      <div className="relative mb-16">
-        <img
-          width={1905}
-          height={700}
-          src="/images/festival.jpg"
-          alt="Festival Image"
-          className="absolute inset-0 object-cover w-full !h-full"
-        />
-        <div className="relative bg-orange-500/80 bg-opacity-90 sm:h-[400px] lg:h-[500px] xl:h-[700px]">
-          <svg
-            className="absolute inset-x-0 text-[#fff7ee] -bottom-1"
-            viewBox="0 0 1160 163"
-          >
-            <path
-              fill="currentColor"
-              d="M-164 13L-104 39.7C-44 66 76 120 196 141C316 162 436 152 556 119.7C676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C76 162.5 -44 162.5 -104 162.5H-164V13Z"
-            />
-          </svg>
-          <div className="relative px-4 py-16 mx-auto overflow-hidden translate-y-10 h-96 md:h-auto md:translate-y-12 xl:translate-y-32 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-            <div className="flex flex-col items-start justify-between xl:flex-row">
-              <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
-                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-[1.2] tracking-tight text-white sm:text-4xl xl:text-6xl lg:text-5xl sm:leading-none">
-                  Here, You can Know Our All{" "}
-                  <span className="font-extrabold text-slate-800/90">
-                    Events
-                  </span>
-                </h2>
-                <p className="max-w-xl mb-4 text-base text-gray-200 md:text-lg">
-                  This is a Testing Website to Show All events, Which Builded By
-                  Next JS and TypeScript.
-                </p>
-              </div>
+    return (
+        <div>
+            <div className="relative mb-16">
+                <img
+                    width={1905}
+                    height={700}
+                    src="/images/festival.jpg"
+                    alt="Festival Image"
+                    className="absolute inset-0 object-cover w-full !h-full"
+                />
+                <div className="relative bg-orange-500/80 bg-opacity-90 sm:h-[400px] lg:h-[500px] xl:h-[700px]">
+                    <svg
+                        className="absolute inset-x-0 text-[#fff7ee] -bottom-1"
+                        viewBox="0 0 1160 163"
+                    >
+                        <path
+                            fill="currentColor"
+                            d="M-164 13L-104 39.7C-44 66 76 120 196 141C316 162 436 152 556 119.7C676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C76 162.5 -44 162.5 -104 162.5H-164V13Z"
+                        />
+                    </svg>
+                    <div className="relative px-4 py-16 mx-auto overflow-hidden translate-y-10 h-96 md:h-auto md:translate-y-12 xl:translate-y-32 sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                        <div className="flex flex-col items-start justify-between xl:flex-row">
+                            <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
+                                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-[1.2] tracking-tight text-white sm:text-4xl xl:text-6xl lg:text-5xl sm:leading-none">
+                                    Here, You can Know Our All{" "}
+                                    <span className="font-extrabold text-slate-800/90">
+                                        Events
+                                    </span>
+                                </h2>
+                                <p className="max-w-xl mb-4 text-base text-gray-200 md:text-lg">
+                                    This is a practical work of a website to
+                                    display all the events, created by Next JS
+                                    and TypeScript.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <SectionHeading>
+                <span className="text-3xl md:text-4xl xl:text-4xl text-slate-800/90 font-luckiestGuy">
+                    Featured <span className="text-orange-500/90">Events</span>
+                </span>
+            </SectionHeading>
+            <EventsList events={events} />
         </div>
-      </div>
-      <SectionHeading>
-        <span className="text-3xl md:text-4xl xl:text-4xl text-slate-800/90 font-luckiestGuy">
-          Featured <span className="text-orange-500/90">Events</span>
-        </span>
-      </SectionHeading>
-      <EventsList events={events} />
-    </div>
-  );
+    );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const featuredEvents: Event[] = getFeaturedEvents();
-  return {
-    props: {
-      events: featuredEvents,
-    },
-  };
+    const featuredEvents: Event[] = getFeaturedEvents();
+    return {
+        props: {
+            events: featuredEvents,
+        },
+    };
 };
 
 export default HomePage;
